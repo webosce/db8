7b551709f5bbd932e7119752e4929014f4bf8837
vinod.sharma <vinod.sharma@lge.com>
2024-05-16 16:00:58 +0530

    db8: Static issue
    :Release Notes:
    Fix to db8 static issue.
    
    :Detailed Notes:
    coverity fix for below CIDs in db8
    9155387
    
    :Testing Performed:
    Locally Tested.
    
    :QA Notes:
    NA
    
    :Issues Addressed:
    [WRQ-20654] Do not attempt to create a std::string from a null pointer
    
    Change-Id: I836f9f9cc207ded7da8dd818e5e305ae8281a197

--- src/luna/MojLunaService.cpp
+++ src/luna/MojLunaService.cpp
@@ -1,4 +1,4 @@
-// Copyright (c) 2009-2021 LG Electronics, Inc.
+// Copyright (c) 2009-2024 LG Electronics, Inc.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -552,6 +552,10 @@ void MojLunaService::Statistic(LSMessage* message)
         char path[100];
         sprintf(path, "/var/db/db8-luna-commands-%d.sh", pid);
         FILE* file = fopen(path, "a+");
+        if (!file) {
+             LOG_DEBUG("Unable to open file.");
+             return;
+        }
         std::string line;
         line = "luna-send -a ";
         line += senderServiceName;
@@ -578,6 +582,11 @@ void MojLunaService::Statistic(LSMessage* message)
         sprintf(path, "/var/db/db8-luna-statistic-%d.txt", pid);
 
         file = fopen(path, "w");
+        if (!file) {
+            LOG_DEBUG("Unable to open file.");
+            return;
+        }
+
         for(const auto& it : m_bytes)
         {
             fputs("Service: ", file);
